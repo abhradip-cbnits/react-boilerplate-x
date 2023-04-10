b3afdcd2b0b31ec80533c4b26cfbc5d4aac16d83
Abhradip Ghosh <aghosh@zuelligpharma.com>
2023-04-10 18:33:11 +0530

    Design fixes done

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,80 +1,81 @@
 import React from "react";
-import {Row, Col} from "antd";
+import {Row, Col, Button} from "antd";
+
 
 const Projects = () => {
     return(
         <>
             <Row>
                 <Col span={12} className="flex justify-center items-center">
-                    <img src={'assets/Source code-bro.png'} style={{height: "70%"}} alt="project-illustration"/>
+                    <img src={'assets/Source code-bro.png'} style={{height: "50%"}} alt="project-illustration"/>
                 </Col>
-                <Col span={12}>
+                <Col span={12} className="pr-14">
                     <Row>
                         <Col span={24}>
-                            <div className="flex flex-row justify-center pb-2">
+                            <div className="flex flex-row justify-center pb-10">
                                 <span>
                                     <p className="body-font font-cookie text-5xl flex justify-center items-center">
                                         My projects
                                     </p>
                                 </span>
                             </div>
-
-                            <span className="flex flex-row justify-center text-s body-font font-roboto-mono">
-                                <p>To know more about my projects,</p>
-                                <p style={{textDecoration : "underline"}}> click here</p>
-                            </span>
                         </Col>
                     </Row>
-
-                    {/* For project listing */}
                     <Row>
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/marketsword.png'} style={{height: "180px"}} alt="marketsworld"/>
-                            </span>
-                        </Col>
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/dpc.png'} style={{height: "30px"}} alt="patient-care-plus"/>
-                            </span>
-                        </Col>
-                    </Row>
+                        <Col span={24}>
+                            <Row className="pb-5">
+                                <Col span={24}>
+                                    <span>
+                                        <p className="body-font font-courier text-base" style={{textAlign: "justify"}}>
+                                            I have been working with Healthcare industries and it's corresponding huge range of
+                                            product set for past 4 years. I have closedly worked on different phases e.g planning and buidling of
+                                            product skeletons, core development, deployement and infrastructure maintenance for a dynamic range of
+                                            product sub-domains like teleconsultation, telemedication, pharmaceutical e-commerce & healthcare analytics.
+                                        </p>
+                                    </span>
+                                </Col>
+                            </Row>
 
-                    <Row className="pb-14">
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/ezrxlogo.png'} style={{height: "40px"}} alt="ezrx"/>
-                            </span>
-                        </Col>
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/knovva.png'} style={{height: "40px"}} alt="knovva-academy"/>
-                            </span>
-                        </Col>
-                    </Row>
+                            <Row className="pb-5">
+                                <Col span={24}>
+                                    <span>
+                                        <p className="body-font font-courier text-base" style={{textAlign: "justify"}}>
+                                            I have worked with Fintech companies to build and maintain products for regulated binary option trading.
+                                        </p>
+                                    </span>
+                                </Col>
+                            </Row>
 
-                    <Row className="pb-14">
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/zola.png'} style={{height: "50px"}} alt="zola"/>
-                            </span>
-                        </Col>
-                        <Col span={12} className="flex justify-center items-center">
-                            <span>
-                                <img src={'assets/ezhealth.png'} style={{height: "30px"}} alt="ezhealth"/>
-                            </span>
+                            <Row className="pb-5">
+                                <Col span={24}>
+                                    <span>
+                                        <p className="body-font font-courier text-base" style={{textAlign: "justify"}}>
+                                            I have worked with Edtech giants to build their globally accessible and scable product for e-learning.
+                                        </p>
+                                    </span>
+                                </Col>
+                            </Row>
+
+                            <Row className="pb-5">
+                                <Col span={24}>
+                                    <span>
+                                        <p className="body-font font-courier text-base" style={{textAlign: "justify"}}>
+                                            I have developed the procurement solution from scratch for heavy engineering industries which is a SEM (Software Enabled Marketplace) in order
+                                            to streamline the supply chain management.
+                                        </p>
+                                    </span>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
 
                     <Row>
-                        <Col span={12} className="flex justify-center items-center">
+                        <Col span={24} className="flex flex-row justify-center items-center">
                             <span>
-                                <img src={'assets/ezpharmacy.png'} style={{height: "30px"}} alt="ezpharmacy"/>
+                                <p>To know more about my projects &nbsp; &nbsp;</p>
                             </span>
-                        </Col>
-                        <Col span={12} className="flex justify-center items-center">
                             <span>
-                                <img src={'assets/catchletter.jpg'} style={{height: "50px"}} alt="catchletter"/>
+                                <Button style={{fontFamily: "cookie", border: "2px solid black", borderRadius: "1px", fontSize: "20px", boxSizing: "initial"}}>Click here </Button>
                             </span>
                         </Col>
                     </Row>
--- src/Components/Publications/Publications.js
+++ src/Components/Publications/Publications.js
@@ -8,7 +8,7 @@ const Publications = () => {
             <Col span={12} className="flex justify-center items-center">
                 <img src={'assets/Certification-bro.png'} style={{height:"450px"}} alt="publication-illustration"/>
             </Col>
-            <Col span={12} type="flex" style={{ alignItems: "center" }} justify="center" gutter={20}>
+            <Col span={12} type="flex" style={{ alignItems: "center" }} justify="center" gutter={20} className="pr-14">
                 <Row>
                     <Col span={24} className="p-4">
                         <p className="body-font font-cookie text-5xl">
@@ -19,7 +19,7 @@ const Publications = () => {
                 <Row>
                     <Col span={24} className="p-4">
                         <span>
-                            <p className="body-font font-courier text-base">Publications on Green Solutions: A Pilot Study on Green Technology and Green Computing</p>
+                            <p className="body-font font-courier text-base" style={{textAlign: "justify"}}>Publications on Green Solutions: A Pilot Study on Green Technology and Green Computing</p>
                         </span>
                     </Col>
                 </Row>
@@ -27,7 +27,7 @@ const Publications = () => {
                 <Row>
                     <Col span={24} className="p-4">
                         <span>
-                            <p className="body-font font-roboto-mono text-xs">Green Solutions: A Pilot Study on Green Technology and Green Computing</p>
+                            <p className="body-font font-roboto-mono text-xs" style={{textAlign: "justify"}}>Green Solutions: A Pilot Study on Green Technology and Green Computing</p>
                         </span>
                     </Col>
                 </Row>
@@ -35,7 +35,7 @@ const Publications = () => {
                 <Row>
                     <Col span={24} className="p-4">
                         <span>
-                            <p className="body-font font-roboto-mono text-xs">International Journal of Advanced Research in Computer Science and Software Engineering. Volume 5, Issue 10, October-2015, ISSN: 2277 128X</p>
+                            <p className="body-font font-roboto-mono text-xs" style={{textAlign: "justify"}}>International Journal of Advanced Research in Computer Science and Software Engineering. Volume 5, Issue 10, October-2015, ISSN: 2277 128X</p>
                         </span>
                     </Col>
                 </Row>
